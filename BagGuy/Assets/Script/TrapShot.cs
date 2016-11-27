546504e762e6820e9a7526080c2894f4a6bbe57b
Alejandro Seguí <alesegdia@gmail.com>
2016-11-27 19:07:24 +0100

    trapshot script for trigger enter

---
+++ BagGuy/Assets/Script/TrapShot.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapShot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (collider.gameObject.layer == LayerMask.NameToLayer ("solid") ) {
+			Destroy (this.gameObject);
+		}
+
+		if (collider.gameObject.layer == LayerMask.NameToLayer ("player")) {
+			GameObject.FindGameObjectWithTag ("bag_guy").GetComponent<BagGuy> ().dead = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
